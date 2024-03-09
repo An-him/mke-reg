adaa768adb7acc2bd309a20353be4eaa5afff19e
Derrick Ampire <ampire90@gmail.com>
2024-03-09 11:44:22 +0300

    list used instead of dict

--- app.py
+++ app.py
@@ -5,23 +5,26 @@ app.config['SECRET_KEY'] = 'your_secret_key'
 
 users = {}
 
+user_id_counter = 1
+
 
 @app.route('/', methods=['GET', 'POST'])
 def registration():
+    global user_id_counter
     if request.method == 'POST':
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        gender = request.form['gender']
-        phone_number = request.form['phone_number']
-        residence = request.form['residence']
+        first_name = request.form.get('first_name', '')
+        last_name = request.form.get('last_name', '')
+        gender = request.form.get('gender', '')
+        phone_number = request.form.get('phone_number', '')
+        residence = request.form.get('residence', '')
         serving = request.form.get('serving', 'no')
         teams = request.form.getlist('team')
         student = request.form.get('student', 'no')
         first_time = request.form.get('first_time', 'no')
-        consent = request.form['consent']
+        consent = request.form.get('consent', '')
 
         # Store the user's information in the database
-        users.append({
+        user_data = {
             'first_name': first_name,
             'last_name': last_name,
             'gender': gender,
@@ -32,7 +35,13 @@ def registration():
             'student': student,
             'first_time': first_time,
             'consent': consent
-        })
+        }
+
+        # Append user data to the users dictionary with a unique identifier
+        users[user_id_counter] = user_data
+
+        # Increment user id counter
+        user_id_counter += 1
 
         return redirect(url_for('thank_you'))
 
